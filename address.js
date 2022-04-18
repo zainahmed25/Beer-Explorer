beea71092972071ed277c6561f99b2aa0f852ff4
zainahmed25 <ahmed.zainu25@gmail.com>
2022-04-17 22:31:07 -0500

    Update address.js

--- address.js
+++ address.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class AddressComponent extends LightningElement {
     @api address;
-
+//This is my learning..
     handleSelect(){
         const addressEev = new CustomEvent(
             'address',
